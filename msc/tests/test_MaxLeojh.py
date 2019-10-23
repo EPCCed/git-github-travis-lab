165e780731112382f8bb15c4e30d6c3655156a29
Jiahui Liu <s1829767@berrendero.inf.ed.ac.uk>
2019-10-23 11:50:05 +0100

    Add another test

--- msc/tests/test_MaxLeojh.py
+++ msc/tests/test_MaxLeojh.py
@@ -4,3 +4,6 @@ from msc.rot13 import rot13
 from msc.rot13 import rot13_char
 def test_rot13_char_a():
     assert "n" == rot13_char("a"), "Unexpected character"
+
+def test_rot13_hello():
+    assert "uryyb" == rot13("hello"), "Unexpected character"
