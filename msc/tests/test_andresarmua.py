1760d47c519f4096475a9cac137b2d9d14f92b04
andresarmua <andresarmua@gmail.com>
2019-10-23 11:45:15 +0100

    helo

--- msc/tests/test_andresarmua.py
+++ msc/tests/test_andresarmua.py
@@ -7,4 +7,6 @@ def test_rot13_char_a():
 def test_rot13_hello():
 	assert "uryyb" == rot13("hello"), "Unexpected string"
 
+def test_rot13_helo():
+	assert "uryb" == rot13("helo"), "Unexpected string"
 
