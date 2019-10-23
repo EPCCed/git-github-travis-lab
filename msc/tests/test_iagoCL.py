a4a0917d4c3050ef0bd4a13b75257b347eb30616
s1000017 <i.calvo-1@sms.ed.ac.uk>
2019-10-23 11:38:22 +0100

    iagoCL's second test for ROT13

--- msc/tests/test_iagoCL.py
+++ msc/tests/test_iagoCL.py
@@ -3,3 +3,6 @@ from msc.rot13 import rot13_char
 
 def test_rot13_char_a():
     assert "n" == rot13_char("a"), "Unexpected character"
+
+def test_rot13_hello():
+    assert "uryyb" == rot13("hello"), "Unexpected result"
